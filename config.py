fd1f674f92d52c0978948f36c6cbc78af5ef25f1
LoveFeelingsWill <147972786+LoveFeelingsWill@users.noreply.github.com>
2023-12-24 11:53:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather on Telegram
-BOT_TOKEN = "5020504303:AAElG7ieBhOGHDJOgO3Ky0qq8ssTRAeWDTo"
+BOT_TOKEN = "5020504303:5348044981:AAHqUVgC-IWXPcax6RdVTUPbLmkbGlrZePI"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "Gitgram"
 
 # If you forked the Repository, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/AnonymousBoy1025/GitGram"
+GIT_REPO_URL = "https://github.com/LoveFeelingsWill/Gitgram"
